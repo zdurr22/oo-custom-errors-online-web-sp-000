93edd0e6ea091aec2c6325da4957d722e6ec7ca5
Zachary Durr <zdurr22@gmail.com>
2020-11-21 00:53:22 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,11 +7,15 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class != Person
-      raise PartnerError
-    else
-      person.partner = self
-    end
+   if person.class != Person
+     begin
+       raise PartnerError
+     rescue PartnerError => error
+         puts error.message
+     end
+   else
+     person.partner = self
+   end
   end
 
   class PartnerError < StandardError
